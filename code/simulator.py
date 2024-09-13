f6d1cdcb279762392d7f61d5e5f7f9b986ba8066
Your Name <you@example.com>
2024-09-13 14:00:29 +0800

    打算贪心重写模拟器

--- code/simulator.py
+++ code/simulator.py
@@ -1,7 +1,6 @@
 import numpy as np
-import random
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import pandas as pd
+from tqdm import tqdm
 
 from q1 import *
 
@@ -13,8 +12,14 @@ class Car:
         self.drive = drive  # 驱动类型
 
         # 保存时间戳位置
+        self.timer = 0
         self.time = []
         self.position = []
+
+    def update_time(self, t_increment, position):
+        self.timer += t_increment
+        self.time.append(self.timer)
+        self.position.append(position)
 
 class TransferMachine:
     def __init__(self, id):
@@ -42,10 +47,10 @@ class PBS:
         self.incar_lane = np.full((6, 10), None) # 进车道
         self.back_lane = np.full((10,), None) # 返回道
 
-        self.time = 0  # 当前时间戳
-        self.end_time = (18*2 + 90) *318
-        self.incar_lane_times = [[0] * 10 for _ in range(6)] # 进车道时间记录
-        self.back_lane_times = [0] * 10 # 返回道时间记录
+        self.hybrid_count = 0
+        self.four_by_four_count = 0
+        self.two_by_two_count = 0
+        self.back_lane_usage = 0
 
     def move_cars(self):
         # 移动进车道中的车
@@ -55,9 +60,7 @@ class PBS:
                     car = self.incar_lane[lane][spot - 1]
                     self.incar_lane[lane][spot] = car
                     self.incar_lane[lane][spot - 1] = None
-                    car.position.append((lane, spot))
-                    car.time.append(self.time)
-                    self.incar_lane_times[lane][spot] = self.time + 9
+                    car.update_time(9, (lane, spot))
 
         # 移动返回道中的车
         for spot in range(9, 0, -1):
@@ -65,70 +68,99 @@ class PBS:
                 car = self.back_lane[spot - 1]
                 self.back_lane[spot] = car
                 self.back_lane[spot - 1] = None
-                car.position.append(('back', spot))
-                car.time.append(self.time)
-                self.back_lane_times[spot] = self.time + 9
+                car.update_time(9, ('back', spot))
+
+    def compute_scores(self, car, lane):
+        score = 100
+
+        # 计算混动车型间隔
+        if car.power == '混动':
+            self.hybrid_count += 1
+        non_hybrid_count = len([c for c in self.incars if c.power != '混动'])
+        if self.hybrid_count != 0 and non_hybrid_count / self.hybrid_count != 2:
+            score -= 1 * 0.4
+
+        # 计算四驱和两驱比例
+        if car.drive == '四驱':
+            self.four_by_four_count += 1
+        elif car.drive == '两驱':
+            self.two_by_two_count += 1
+        if self.four_by_four_count != 0 and self.two_by_two_count / self.four_by_four_count != 1:
+            score -= 1 * 0.3
+
+        # 返回道使用次数
+        if lane == 'back':
+            self.back_lane_usage += 1
+            score -= self.back_lane_usage * 0.2
+
+        # 总调度时间
+        total_time = car.timer + [18, 12, 6, 0, 12, 18][lane]
+        theoretical_time = 9 * len(self.incars) + 72
+        time_penalty = 0.01 * (total_time - theoretical_time)
+        score -= time_penalty * 0.1
+
+        return score
 
     def simulator(self):
-        while self.time < self.end_time:
+        priority_order = [4, 3, 2, 5, 1, 6]  # 优先顺序
+
+        total = len(self.incars)
+        for i, it in tqdm(enumerate(self.incars), total=total):
             # 处理返回道10停车位的车
             if self.back_lane[9] is not None and self.reception_machine.idle:
                 car = self.back_lane[9]
                 self.reception_machine.load_car(car)
                 self.back_lane[9] = None
-                car.time.append(self.time)
-                car.position.append('reception_machine')
+                car.update_time(0, 'reception_machine')
 
-                # 将车移动到进车道10停车位
-                for lane in range(6):
-                    if self.incar_lane[lane][9] is None:
-                        self.incar_lane[lane][9] = self.reception_machine.unload_car()
-                        self.time += [24, 18, 12, 6, 12, 18][lane]
-                        self.incar_lane_times[lane][9] = self.time
-                        car.time.append(self.time)
-                        car.position.append((lane, 9))
+                best_lane = None
+                for lane in priority_order:
+                    lane_index = lane - 1
+                    if self.incar_lane[lane_index][9] is None:
+                        best_lane = lane_index
                         break
+
+                if best_lane is not None:
+                    self.incar_lane[best_lane][9] = self.reception_machine.unload_car()
+                    car.update_time([24, 18, 12, 6, 12, 18][best_lane], (best_lane, 9))
 
             # 处理进车序列中的车
             if self.incars and self.reception_machine.idle:
                 car = self.incars.pop(0)
                 self.reception_machine.load_car(car)
-                car.time.append(self.time)
-                car.position.append('reception_machine')
+                car.update_time(0, 'reception_machine')
 
-                # 将车移动到进车道10停车位
-                for lane in range(6):
-                    if self.incar_lane[lane][9] is None:
-                        self.incar_lane[lane][9] = self.reception_machine.unload_car()
-                        self.time += [18, 12, 6, 0, 12, 18][lane]
-                        self.incar_lane_times[lane][9] = self.time
-                        car.time.append(self.time)
-                        car.position.append((lane, 9))
+                best_lane = None
+                for lane in priority_order:
+                    lane_index = lane - 1
+                    if self.incar_lane[lane_index][9] is None:
+                        best_lane = lane_index
                         break
 
-            # 处理进车道1停车位的车
-            for lane in range(6):
-                if self.incar_lane[lane][0] is not None and self.shipment_machine.idle:
-                    car = self.incar_lane[lane][0]
-                    self.shipment_machine.load_car(car)
-                    self.incar_lane[lane][0] = None
-                    car.time.append(self.time)
-                    car.position.append('shipment_machine')
+                if best_lane is not None:
+                    self.incar_lane[best_lane][9] = self.reception_machine.unload_car()
+                    car.update_time([18, 12, 6, 0, 12, 18][best_lane], (best_lane, 9))
 
-                    # 将车移动到总装接车口
-                    self.time += [18, 12, 6, 0, 12, 18][lane]
+            # 处理进车道1停车位的车
+            for lane in priority_order:
+                lane_index = lane - 1
+                if self.incar_lane[lane_index][0] is not None and self.shipment_machine.idle:
+                    car = self.incar_lane[lane_index][0]
+                    self.shipment_machine.load_car(car)
+                    self.incar_lane[lane_index][0] = None
+                    car.update_time(0, 'shipment_machine')
+
+                    car.update_time([18, 12, 6, 0, 12, 18][lane_index], 'output')
                     self.shipment_machine.unload_car()
-                    car.time.append(self.time)
-                    car.position.append('output')
                     self.outcars.append(car)
                     break
 
             self.move_cars()
-            self.time += 1
-            print(f"Time:{self.time}")
+
+            if i == 300:
+                print(f"self.outcars：{self.outcars}")
 
 if __name__ == '__main__':
-    # 示例数据
     incars = read_data('附件1.xlsx')
 
     incars = [Car(incar["进车顺序"], incar["车型"], incar["动力"], incar["驱动"]) for i, incar in incars.iterrows()]
@@ -142,20 +174,3 @@ if __name__ == '__main__':
         print(f'Car {car.car_id} movement history:')
         for t, pos in zip(car.time, car.position):
             print(f'  Time {t}: Position {pos}')
-
-
-# def animate(state):
-#     plt.clf()
-#     plt.title(f"Timestamp: {state}")
-#     for lane in range(6):
-#         for position in range(10):
-#             car = pbs.incar_lane[lane, position]
-#             if car:
-#                 plt.scatter(lane, position, color='blue', s=100)
-#     for position in range(10):
-#         car = pbs.back_lane[position]
-#         if car:
-#             plt.scatter(6, position, color='red', s=100)
-#     plt.xlim(-1, 7)
-#     plt.ylim(-1, 10)
-#     plt.pause(0.1)
