ad45a68d0a962540dd5ad34dea1f0d5b218509d9
Your Name <you@example.com>
2024-09-13 22:07:56 +0800

    全局计时ok。in区，out区交界处计时有问题。

--- code/simulator.py
+++ code/simulator.py
@@ -12,14 +12,13 @@ class Car:
         self.power = power  # 动力类型
         self.drive = drive  # 驱动类型
 
-        # 保存时间戳位置
-        self.timer = 0
+        # 保存时间戳和位置信息
         self.time = []
         self.position = []
 
-    def update_state(self, t_increment, position):
-        self.timer += t_increment
-        self.time.append(self.timer)
+    def update_state(self, time, position):
+        # 更新汽车的位置信息和对应的全局时间
+        self.time.append(time)
         self.position.append(position)
 
 class TransferMachine:
@@ -27,6 +26,8 @@ class TransferMachine:
         self.machine_id = id  # 横移机ID
         self.current_car = None  # 当前正在运送的车身
         self.idle = True
+        self.position = 'middle'  # 初始位置为中间位置
+        self.next_available_time = 0  # 下次可用时间
 
     def load_car(self, car):
         self.current_car = car
@@ -46,6 +47,7 @@ class PBS:
         self.incar_lane = np.full((6, 10), None) # 进车道
         self.back_lane = np.full((10,), None) # 返回道
 
+        self.global_timer = 0  # 全局计时器
         self.hybrid_count = 0
         self.four_by_four_count = 0
         self.two_by_two_count = 0
@@ -56,70 +58,24 @@ class PBS:
 
         sim_flag = True
         while sim_flag:
-            # in区：入车队列进车, 按优先级贪心选择车道1停车位
-            if self.incars:
-                car = copy.deepcopy(self.incars.pop(0))
-                # print(f"car.id:{car.id}")
-            self.in_machine.load_car(car)
-            car.update_state(0, 'in')
-            self.in_machine.unload_car()
-            for lane in priority_order:
-                if not self.incar_lane[lane][9]:
-                    choice_lane = lane
-                    break
-            car.update_state([18, 12, 6, 0, 12, 18][choice_lane], (choice_lane, 9))
-            self.outcars[car.id - 1] = car
-            self.incar_lane[choice_lane][9] = car
+            # 在每一轮模拟中将全局时间加1
+            self.global_timer += 1
 
-            # in区：当返回道10停车位有车身，同时接车横移机空闲时，优先处理返回道10停车位上的车身
-            if self.back_lane[9] and self.in_machine.idle:
-                car = self.back_lane[9]
-                self.in_machine.load_car(car)
-                car.update_state(0, 'in')
-                self.in_machine.unload_car()
-                for lane in priority_order:
-                    if not self.incar_lane[lane][9]:
-                        choice_lane = lane
-                        break
-                car.update_state([24, 18, 12, 6, 12, 18][choice_lane], (choice_lane, 9))
-                self.outcars[car.id - 1] = car
-                self.back_lane[9] = None
-            
-            # out区：当若干进车道1停车位有车身等候，同时送车横移机空闲时，优先处理最先到达1停车位的车身
-            cars = [
-                self.incar_lane[i][0]
-                for i in range(self.incar_lane.shape[0]) 
-                if self.incar_lane[i][0] and self.out_machine.idle
-            ]
+            # 处理车身移动
+            if self.global_timer % 9 == 0:
+                self.move_cars()
 
-            if cars:
-                cars = sorted(cars, key=lambda car: car.time)
-                for car in cars:
-                    self.out_machine.load_car(car)
-                    self.out_machine.unload_car()
-                    self.incar_lane[car.position[-1][0]][car.position[-1][1]] = None
-                    car.update_state([18, 12, 6, 0, 12, 18][car.position[-1][0]], 'out')
-                    self.outcars[car.id - 1] = car
-            
-            # 检查车道移动：当某车身所在停车位的下一停车位出现空位时，车身必须立即开始向下一停车位移动
-            self.move_cars()
+            # 处理接车横移机任务
+            if self.in_machine.idle and self.global_timer >= self.in_machine.next_available_time:
+                self.handle_in_machine(priority_order)
 
-            # out区：处理需要进行调度返回的车
-
-
-            # 约束：如果任意进车道1停车位有车身，那么送车横移机不能设置为空闲状态
-            for i in range(self.incar_lane.shape[0]):
-                if self.incar_lane[0][i]:
-                    self.out_machine.idle = False
-
-            # 车道移动：当某车身所在停车位的下一停车位出现空位时，车身必须立即开始向下一停车位移动
-            self.move_cars()
+            # 处理送车横移机任务
+            if self.out_machine.idle and self.global_timer >= self.out_machine.next_available_time:
+                self.handle_out_machine(priority_order)
 
             # 检查所有汽车是否全部送出, 全送出结束循环
             out_count = sum(1 for car in self.outcars if car is not None and car.position[-1] == 'out')
-            print(f"out_count:{out_count}")
             sim_flag = out_count < len(self.outcars) 
-
 
     def move_cars(self):
         # 移动进车道中的车
@@ -129,7 +85,7 @@ class PBS:
                     car = self.incar_lane[lane][spot + 1]
                     self.incar_lane[lane][spot] = car
                     self.incar_lane[lane][spot + 1] = None
-                    car.update_state(9, (lane, spot))
+                    car.update_state(self.global_timer, (lane, spot))
                     self.outcars[car.id - 1] = car
 
         # 移动返回道中的车
@@ -138,8 +94,65 @@ class PBS:
                 car = self.back_lane[spot - 1]
                 self.back_lane[spot] = car
                 self.back_lane[spot - 1] = None
-                car.update_state(9, ('back', spot))
+                car.update_state(self.global_timer, ('back', spot))
                 self.outcars[car.id - 1] = car
+
+    def handle_in_machine(self, priority_order):
+        # in区：入车队列进车, 按优先级贪心选择车道10停车位
+        if self.incars:
+            car = copy.deepcopy(self.incars.pop(0))
+            self.in_machine.load_car(car)
+            car.update_state(self.global_timer, 'in')
+            self.in_machine.unload_car()
+            for lane in priority_order:
+                if not self.incar_lane[lane][9]:
+                    choice_lane = lane
+                    break
+            time_cost = [18, 12, 6, 0, 12, 18][choice_lane]
+            car.update_state(self.global_timer + time_cost, (choice_lane, 9))
+            self.in_machine.next_available_time = self.global_timer + time_cost
+            self.outcars[car.id - 1] = car
+            self.incar_lane[choice_lane][9] = car
+
+        # in区：当返回道10停车位有车身，同时接车横移机空闲时，优先处理返回道10停车位上的车身
+        elif self.back_lane[9]:
+            car = self.back_lane[9]
+            self.in_machine.load_car(car)
+            car.update_state(self.global_timer, 'in')
+            self.in_machine.unload_car()
+            for lane in priority_order:
+                if not self.incar_lane[lane][9]:
+                    choice_lane = lane
+                    break
+            time_cost = [24, 18, 12, 6, 12, 18][choice_lane]
+            car.update_state(self.global_timer + time_cost, (choice_lane, 9))
+            self.in_machine.next_available_time = self.global_timer + time_cost
+            self.outcars[car.id - 1] = car
+            self.back_lane[9] = None
+
+    def handle_out_machine(self, priority_order):
+        # out区：当若干进车道1停车位有车身等候，同时送车横移机空闲时，优先处理最先到达1停车位的车身
+        cars = [
+            self.incar_lane[i][0]
+            for i in range(self.incar_lane.shape[0]) 
+            if self.incar_lane[i][0]
+        ]
+
+        if cars:
+            cars = sorted(cars, key=lambda car: car.time[-1])
+            for car in cars:
+                self.out_machine.load_car(car)
+                self.out_machine.unload_car()
+                self.incar_lane[car.position[-1][0]][car.position[-1][1]] = None
+                time_cost = [18, 12, 6, 0, 12, 18][car.position[-1][0]]
+                car.update_state(self.global_timer + time_cost, 'out')
+                self.out_machine.next_available_time = self.global_timer + time_cost
+                self.outcars[car.id - 1] = car
+
+        # 约束：如果任意进车道1停车位有车身，那么送车横移机不能设置为空闲状态
+        for i in range(self.incar_lane.shape[0]):
+            if self.incar_lane[i][0]:
+                self.out_machine.idle = False
 
     def compute_scores(self, car, lane):
         score = 100
@@ -164,12 +177,6 @@ class PBS:
             self.back_lane_usage += 1
             score -= self.back_lane_usage * 0.2
 
-        # 总调度时间
-        total_time = car.timer + [18, 12, 6, 0, 12, 18][lane]
-        theoretical_time = 9 * len(self.incars) + 72
-        time_penalty = 0.01 * (total_time - theoretical_time)
-        score -= time_penalty * 0.1
-
         return score
 
 if __name__ == '__main__':
@@ -183,6 +190,6 @@ if __name__ == '__main__':
 
     # Print car movement history
     for car in pbs.outcars:
-        print(f'Car {car.car_id} movement history:')
+        print(f'Car {car.id} movement history:')
         for t, pos in zip(car.time, car.position):
             print(f'  Time {t}: Position {pos}')
